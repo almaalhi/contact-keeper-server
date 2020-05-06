daa33a41e5e0385cfa3e123ac81e39580152e626
Alex Alvarado <alex.alvarad@gmail.com>
2020-05-06 12:05:26 -0700

    Add logout functionality

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -96,7 +96,10 @@ const AuthState = (props) => {
   };
 
   // Logout
-  const logout = () => console.log('logout');
+  const logout = () =>
+    dispatch({
+      type: LOGOUT,
+    });
 
   // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
